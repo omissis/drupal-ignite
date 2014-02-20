f8a5dab2b66f174e82477bfb64e24d3d8ba85c74
omissis <claudi0.beatric3@gmail.com>
2014-02-20 01:25:44 +0100

    slightly improved drush aliases files and commented dev and stage out, leaving them to the user to enable

--- template/sites/all/drush/aliases.drushrc.php
+++ template/sites/all/drush/aliases.drushrc.php
@@ -4,18 +4,23 @@
  * Define drush aliases for the current project.
  */
 
-// Define home directory.
-$home_directory = __DIR__ . '/../../../';
+/**
+ * Alias for local environment.
+ *
+ * Assumes <site-name>_loc/<site-name>_loc as username/password for the database
+ * and admin/admin for the website administrator username/password. YOU SHOULD CHANGE THIS.
+ */
 
-// Create aliases.
-$aliases['__site__.loc'] = array(
-  'uri' => 'loc.__site__.__tld__',
-  'root' => $home_directory . '/Sites/__vendor__/__site__',
-  'db-url' => 'mysql://__site___loc:__site___loc@localhost/__site___loc',
+$loc_db_url = 'mysql://__site___loc:__site___loc@127.0.0.1/__vendor_____site___loc';
+
+$aliases['__vendor__.__site__.loc'] = array(
+  'uri' => 'loc.__site__.__vendor__.__tld__',
+  'root' => '__docroot__',
+  'db-url' => $loc_db_url,
   'path-aliases' => array(
-    '%drush' => $home_directory . '/pear/bin',
-    '%drush-script' => $home_directory . '/pear/bin/drush',
-    '%dump-dir' => $home_directory . '/Sites/__vendor__/__site__/dumps',
+    '%drush' => '__docroot__/vendor/drush/drush',
+    '%drush-script' => '__docroot__/bin/drush',
+    '%dump-dir' => '__docroot__/dumps',
     '%files' => 'sites/default/files',
   ),
   'target-command-specific' => array(
@@ -26,35 +31,45 @@ $aliases['__site__.loc'] = array(
   'command-specific' => array(
     'site-install' => array(
       'site-name' => ucfirst('__site__'),
-      'site-mail' => 'info@loc.__site__.__tld__',
-      'db-url'    => 'mysql://__site___loc:__site___loc@127.0.0.1/__site__',
-      'account-mail' => 'info@loc.__site__.__tld__',
+      'site-mail' => 'info@loc.__site__.__vendor__.__tld__',
+      'db-url'    => $loc_db_url,
+      'account-mail' => 'admin@loc.__site__.__vendor__.__tld__',
       'account-name' => 'admin',
       'account-pass' => 'admin',
     ),
   ),
 );
 
-$aliases['__site__.dev'] = array(
-  'uri' => 'dev.__site__.__vendor__.__tld__',
-  'root' => '/var/www/dev.__site__.__vendor__.__tld__',
-  'remote-host' => 'dev.__site__.__vendor__.__tld__',
-  'remote-user' => 'root',
-  'command-specific' => array(
-    'sql-sync' => array(
-      'no-cache' => TRUE,
-    ),
-  ),
-);
+/**
+ * Alias for development environment.
+ *
+ * You should at least ensure 'root' path is correct.
+ */
+// $aliases['__vendor__.__site__.dev'] = array(
+//   'uri' => 'dev.__site__.__vendor__.__tld__',
+//   'root' => '/var/www/__vendor__/__site__/dev',
+//   'remote-host' => 'dev.__site__.__vendor__.__tld__',
+//   'remote-user' => 'root',
+//   'command-specific' => array(
+//     'sql-sync' => array(
+//       'no-cache' => TRUE,
+//     ),
+//   ),
+// );
 
-$aliases['__site__.stage'] = array(
-  'uri' => 'stage.__site__.__vendor__.__tld__',
-  'root' => '/var/www/stage.__site__.__vendor__.__tld__',
-  'remote-host' => 'stage.__site__.__vendor__.__tld__',
-  'remote-user' => 'root',
-  'command-specific' => array(
-    'sql-sync' => array(
-      'no-cache' => TRUE,
-    ),
-  ),
-);
+/**
+ * Alias for staging environment.
+ *
+ * You should at least ensure 'root' path is correct.
+ */
+// $aliases['__vendor__.__site__.stage'] = array(
+//   'uri' => 'stage.__site__.__vendor__.__tld__',
+//   'root' => '/var/www/__vendor__/__site__/stage',
+//   'remote-host' => 'stage.__site__.__vendor__.__tld__',
+//   'remote-user' => 'root',
+//   'command-specific' => array(
+//     'sql-sync' => array(
+//       'no-cache' => TRUE,
+//     ),
+//   ),
+// );
