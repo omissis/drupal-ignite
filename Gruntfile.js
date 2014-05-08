5e25ec1053481983919339a9682cac405b41bb4b
omissis <claudi0.beatric3@gmail.com>
2014-05-08 19:14:55 +0200

    added google analytics

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
             },
             build: {
                 files: {
-                    'assets/app.min.js' : ["js/jquery.utils.js", "js/jquery.sticky.js", "js/config.js"]
+                    'assets/app.min.js' : ["js/ga.js", "js/jquery.utils.js", "js/jquery.sticky.js", "js/config.js"]
                 }
             }
         },
